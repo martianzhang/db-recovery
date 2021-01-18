73991f97ed0d3e4db5a03fc3ba2d609ea107d4ec
Leon Zhang <martianzhang@hotmail.com>
2021-01-19 00:17:23 +0800

    pass mysql-version flag into test case
    go test github.com/zbdba/db-recovery/recovery/redo -mysql-version 5.x -v -timeout 30s -run TestLogHeader

--- recovery/ibdata/parse_test.go
+++ recovery/ibdata/parse_test.go
@@ -10,18 +10,24 @@ import (
 var p *Parse
 var devPath string
 var fixturePath string
+
+// go test -mysql-version 5.6 -v -timeout 30s -run TestFunc
 var mysqlRelease = flag.String("mysql-release", "mysql", "mysql docker image release vendor, eg. mysql, percona, mariadb")
 var mysqlVersion = flag.String("mysql-version", "5.7", "mysql docker image versions, eg. 5.7, 8.0")
 
-func init() {
+func TestMain(m *testing.M) {
 	p = NewParse()
 	_, filename, _, _ := runtime.Caller(0)
+
+	flag.Parse()
 	devPath = filepath.Dir(filepath.Dir(filepath.Dir(filename)))
 	fixturePath = devPath + "/cmd/test/fixture/" + *mysqlRelease + "_" + *mysqlVersion
 
 	// set logs to stderr, and log-level = trace
 	flag.Set("logtostderr", "true")
 	flag.Set("v", "5")
+
+	m.Run()
 }
 
 // go test -v -timeout 30s -run ^TestParse$ github.com/zbdba/db-recovery/recovery/ibdata > TestParse.log 2>&1
--- recovery/logs/logs_test.go
+++ recovery/logs/logs_test.go
@@ -9,14 +9,20 @@ import (
 
 var devPath string
 var fixturePath string
+
+// go test -mysql-version 5.6 -v -timeout 30s -run TestFunc
 var mysqlRelease = flag.String("mysql-release", "mysql", "mysql docker image release vendor, eg. mysql, percona, mariadb")
 var mysqlVersion = flag.String("mysql-version", "5.7", "mysql docker image versions, eg. 5.7, 8.0")
 
-func init() {
+func TestMain(m *testing.M) {
 	_, filename, _, _ := runtime.Caller(0)
+
+	flag.Parse()
 	devPath = filepath.Dir(filepath.Dir(filepath.Dir(filename)))
 	fixturePath = devPath + "/cmd/test/fixture/" + *mysqlRelease + "_" + *mysqlVersion
 	InitLogs(devPath+"/cmd/test/fixture/", "debug")
+
+	m.Run()
 }
 
 func TestDebug(t *testing.T) {
